bd2570f84b040b66289418486c5dce510e3cdf7a
albertatwisoft <albert.davis@wisoftsolutions.com>
2025-05-23 18:11:33 +0400

    updates....

--- src/api/service/controllers/service.js
+++ src/api/service/controllers/service.js
@@ -1,9 +1,33 @@
 'use strict';
 
-/**
- * service controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::service.service');
+module.exports = createCoreController('api::service.service', ({ strapi }) => ({
+  async find(ctx) {
+    const { slug } = ctx.query;
+
+
+
+    // If slug param is present, search by slug
+    if (slug) {
+
+      const entity = await strapi.entityService.findMany('api::service.service', {
+        filters: { Slug: { $eq: slug } },
+        populate: ctx.query.populate || '*',
+      });
+
+          console.log('Query params:', entity,slug);
+
+      if (!entity || entity.length === 0) {
+        return ctx.notFound('No service found with this slug');
+      }
+
+      // Return just that one entity
+      const sanitized = await this.sanitizeOutput(entity[0], ctx);
+      return this.transformResponse(sanitized);
+    }
+
+    // Default: return all services
+    return super.find(ctx);
+  }
+}));
