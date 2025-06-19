c79be42b71a20f0e9afcf6ea76d55d4427082a92
albertatwisoft <albert.davis@wisoftsolutions.com>
2025-06-19 15:51:04 +0400

    updates..

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -6,4 +6,42 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::page.page');
+module.exports = createCoreController('api::page.page', ({ strapi }) => ({
+
+  async find(ctx) {
+    const { slug, id, populate = '*' } = ctx.query;
+
+    // If slug is provided, fetch by slug
+    if (slug) {
+      const entity = await strapi.entityService.findMany('api::page.page', {
+        filters: { slug: { $eq: slug } },
+        populate,
+      });
+
+      if (!entity || entity.length === 0) {
+        return ctx.notFound(`No page found with slug: ${slug}`);
+      }
+
+      const sanitized = await this.sanitizeOutput(entity[0], ctx);
+      return this.transformResponse(sanitized);
+    }
+
+    // If ID is provided, fetch by ID
+    if (id) {
+      const entity = await strapi.entityService.findOne('api::page.page', id, {
+        populate,
+      });
+
+      if (!entity) {
+        return ctx.notFound(`No page found with id: ${id}`);
+      }
+
+      const sanitized = await this.sanitizeOutput(entity, ctx);
+      return this.transformResponse(sanitized);
+    }
+
+    // Default behavior: return all pages
+    return super.find(ctx);
+  },
+
+}));
